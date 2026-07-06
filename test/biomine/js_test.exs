e572e0dd5c69c42f1c76cbeea5c85363b2022e80
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2026-07-06 14:17:18 +0700

    Make parameterize consistent and update gitignore
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- test/biomine/js_test.exs
+++ test/biomine/js_test.exs
@@ -3,20 +3,20 @@ defmodule Biomine.JSTest do
     async: true,
     parameterize: [
       %{
-        input: "let x=1",
+        source: "let x=1",
         output: """
         let x = 1;
         """
       },
       %{
-        input: "let x:string='hello'",
+        source: "let x:string='hello'",
         output: """
         let x: string = "hello";
         """
       }
     ]
 
-  test "format_js", %{input: input, output: output} do
-    assert {:ok, ^output} = Biomine.format_js(input)
+  test "format_js", %{source: source, output: output} do
+    assert {:ok, ^output} = Biomine.format_js(source)
   end
 end
