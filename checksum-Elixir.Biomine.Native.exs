263e67406e5aa792b9f8cae386ba0c5a2f1ce937
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2026-07-08 21:13:58 +0700

    Generate checksum for 0.3.0
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- checksum-Elixir.Biomine.Native.exs
+++ checksum-Elixir.Biomine.Native.exs
@@ -1,10 +1,10 @@
 %{
-  "biomine_native-v0.2.2-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:a92029dff80c8c06d6f1755cbf24cd2b1349dbb3f8418e4edb07017f92438a3d",
-  "biomine_native-v0.2.2-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:ab7ca0cb4fbc5c5e7791fa963cee4031e6f89607c2b35773eb8300f3cfce0547",
-  "libbiomine_native-v0.2.2-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:c78dd6f4c866ca0691647559e8a312c5e90f98b3d2b98864b7d077aa4489ef8e",
-  "libbiomine_native-v0.2.2-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:109add87674ff691637902af83daeda804518aebe3bc5a746daa25550f4a446b",
-  "libbiomine_native-v0.2.2-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:58b1a065ea1d930d3da3da00748f2ac8dd3cb0d9c9cf088ebd23ce8714cf6b24",
-  "libbiomine_native-v0.2.2-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:087620b3672adc5d962158c78d1c8a771b884a46d1c9fd9604da68f607149442",
-  "libbiomine_native-v0.2.2-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:1a87433bc86bd946a680d1b5b65f40818f8e2e9666e3ac06663f904518586343",
-  "libbiomine_native-v0.2.2-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:5cb6e1c9de9283d55386896650a86a4842859e6aed9f18f65348d12283f5b9fc",
+  "biomine_native-v0.3.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:f585e0cc2181f6b3975a20090187fdd50ee551da35f7d351a37b3136310fc4d0",
+  "biomine_native-v0.3.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:35a4727c8707426bf12fbfe6a78b4a183c4fdfe1a015bcd04a4f95291d3202e3",
+  "libbiomine_native-v0.3.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:18360913ce9e28a986ad0d26c796ac7b1eb0c92d35bceb9164a8a5d70afa5d8c",
+  "libbiomine_native-v0.3.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:6c9e3e66b82113468fde1eab84e341caed5ebf6b3896e704cd36746a8a52c9b0",
+  "libbiomine_native-v0.3.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:f14f201553ed1db5fa51431e7c1c6d49cbf280740d30103ccf5d0e4818ef0e58",
+  "libbiomine_native-v0.3.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:f0304e255b24840af66cdedd16da967b86ec444ca003b94eec8a75dc7e2ee467",
+  "libbiomine_native-v0.3.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:573f4cf8c5eafabe426ef26cb68da4315c8b9036cd2d53f4e9f00d41a6112e3f",
+  "libbiomine_native-v0.3.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:46c4b24c3b56f677544dd33151596b07acc36d876fe4d08c4d1a4d5eb50a98e2",
 }
